3a06a8a758329c778aadfd0494f23deb600ad928
arun-achilles <arunkumarb@ArunKumartekiMacBook-Pro.local>
2021-06-25 17:23:40 +0530

    [Add google login]

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { withCookies } from 'react-cookie';
+import { GoogleLogin } from 'react-google-login';
+
 
 class Login extends Component {
     state = {
@@ -67,6 +69,26 @@ class Login extends Component {
     }
 
     render () {
+      const responseGoogle = (response) => {
+        fetch(`${process.env.REACT_APP_API_URL}/rest-auth/google/`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({access_token: response.tokenObj.access_token})
+        }).then( resp => resp.json())
+        .then( res => {
+            if (res.hasOwnProperty('key')){
+              this.props.cookies.set('mr-token', res.key)
+              window.location.href = '/movies'
+            } else {
+              alert(JSON.stringify(res))
+            }
+        })
+        .catch( err => console.log(err))           
+        
+      }        
+  
         return (
             <div className="login-container">
                 <h1>
@@ -78,6 +100,14 @@ class Login extends Component {
                 <input type="password" name="password" value={this.state.credentials.password} onChange={this.inputChanged} />
                 <button onClick={this.login}>{this.state.isLoginView ? 'Login' : 'Register'}</button>
                 <p onClick={this.toggleView}>{this.state.isLoginView ? 'Create an Account?' : 'Already have an account?'}</p>
+                <GoogleLogin
+                  clientId="416898136690-lded9q8bqb53qqgmi6so9pqiogu73oqd.apps.googleusercontent.com"
+                  buttonText="Login"
+                  onSuccess={responseGoogle}
+                  onFailure={responseGoogle}
+                  cookiePolicy={'single_host_origin'}
+                />
+
             </div>
         )
     }
